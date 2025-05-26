c8a9ea492c6cc6e058cbd8c9c855d0db32de626c
gboy <seojinhyeon9@gmail.com>
2025-05-26 19:04:44 +0900

    7th

--- assignment7/Poketball Sample/Assets/Scripts/UIManager.cs
+++ assignment7/Poketball Sample/Assets/Scripts/UIManager.cs
@@ -20,7 +20,8 @@ public class UIManager : MonoBehaviour
     {
         // NowCoroutine이 있다면 멈추고 새로운 DisplayTextCoroutine을 시작한다.
         // ---------- TODO ---------- 
-        
+        if (NowCoroutine != null) StopCoroutine(NowCoroutine);
+        NowCoroutine = StartCoroutine(DisplayTextCoroutine(text, duration));
         // -------------------- 
     }
 
@@ -28,7 +29,9 @@ public class UIManager : MonoBehaviour
     {
         // MyText에 text를 duration초 동안 띄운다.
         // ---------- TODO ---------- 
-        yield return null; // dummy, remove this
+        MyText.text = text;
+        yield return new WaitForSeconds(duration);
+        MyText.text = "";
         // -------------------- 
     }
 }
